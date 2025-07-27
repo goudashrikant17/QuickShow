127347d3a21a6ac9cf793f564cebcf305b062998
Shrikant Gouda <goudashrikant17@gmail.com>
2025-07-27 21:50:39 +0530

    remove some line

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,6 @@ const Navbar = () => {
 
   return (
     <>
-{/* NAVBAR STARTED FROM HERE */}
       <div className=" fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5 ">
         <Link to="/">
           <img src={assets.logo} alt="LOGO" className="w-36 h-auto" />
