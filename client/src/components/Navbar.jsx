b85fb970d20614ed1e77472733884ade453fdf43
Shrikant Gouda <goudashrikant17@gmail.com>
2025-07-27 13:31:51 +0530

    added some extra  lines

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,6 +12,7 @@ const Navbar = () => {
 
   return (
     <>
+    {/* NAVBAR STARTED HERE */}
       <div className=" fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5 ">
         <Link to="/">
           <img src={assets.logo} alt="LOGO" className="w-36 h-auto" />
